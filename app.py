846f5d675e4eb9c2c243fb64887fb0f2cdcf81a5
Maciej <macglowacki@gmail.com>
2026-05-10 18:15:09 +0200

    Dodano menu i walidacje pustego koszyka

--- app.py
+++ app.py
@@ -37,11 +37,55 @@ class Koszyk:
         print(f"Suma: {self.suma} zł")
 
 
+def menu(sklep, koszyk):
+    while True:
+        print(f"\n--- Sklep {sklep.nazwa} ---")
+        print("1. Pokaż produkty w sklepie")
+        print("2. Dodaj produkt do koszyka")
+        print("3. Pokaż koszyk")
+        print("4. Wyjdź")
+
+        try:
+            choice = int(input("Wybierz: "))
+        except ValueError:
+            print("Tylko cyfry")
+            continue
+
+        if choice == 1:
+            if not sklep.produkty:
+                print("Brak produktów w sklepie")
+            else:
+                sklep.pokaz_produkty()
+        elif choice == 2:
+            if not sklep.produkty:
+                print("Brak produktów w sklepie")
+            else:
+                sklep.pokaz_produkty()
+                try:
+                    nr = int(input("Podaj numer: ")) - 1
+                    ilosc = int(input("Podaj ilosc: "))
+                except ValueError:
+                    print("Tylko cyfry")
+                    continue
+
+                koszyk.dodaj_do_koszyka(sklep.produkty[nr], ilosc)
+                print("Dodano do koszyka")
+        elif choice == 3:
+            if not koszyk.produkty:
+                print("Koszyk jest pusty")
+            else:
+                koszyk.pokaz_koszyk()
+        elif choice == 4:
+            print("Zamykanie...")
+            break
+        else:
+            print("Tylko cyfry 1-4")
+            continue
+
+
 sklep = Sklep("Media Expert")
 sklep.dodaj_produkt("Iphone", 2999, 10)
 sklep.dodaj_produkt("Sony XMH 4500", 999, 10)
 
 koszyk = Koszyk()
-koszyk.dodaj_do_koszyka(sklep.produkty[0], 2)
-koszyk.dodaj_do_koszyka(sklep.produkty[1], 1)
-koszyk.pokaz_koszyk()
+menu(sklep, koszyk)
